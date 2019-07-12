244ffe4dc7c6e060803abd01a03ea7d39f9fed21
Three Q <threeq1818@gmail.com>
2019-07-12 14:53:19 +0800

    install redux pkgs and made reducer/index

---
+++ src/reducer/index.ts
@@ -0,0 +1,12 @@
+// index.ts
+
+import { combineReducers } from 'redux';
+import errorReducer from './errorReducer';
+import authReducer from './authReducer';
+import characterReducer from './characterReducer';
+
+export default combineReducers({
+    errors: errorReducer,
+    auth: authReducer,
+    character: characterReducer
+});
